6140f91596b58340dfded02ba246b5917d045911
HttpAnimation <sahwndouglasyt@gmail.com>
2024-04-09 23:31:34 -0700

    full screen

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "macOS-like Desktop");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
-
+    gtk_window_fullscreen(GTK_WINDOW(window));  
     // Create a vertical box to hold the menu and possibly other widgets
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
